676d79605e8b63b71addc534ce91940febd262ac
Hugo <hugo.umbert@estiam.com>
2021-07-05 17:08:05 +0200

    start header

---
+++ layouts/header.php
@@ -0,0 +1,11 @@
+<header>
+
+    <nav>
+        <div class="left_bloc">
+            <a href="">Home</a>
+        </div>
+        <div class="reight_bloc">
+            <a href="">Connexion</a>
+        </div>
+    </nav>
+</header>
